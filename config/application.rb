ed9024849fc40603e027ec943d5eccb6cc55bfe2
megancallenbach <megancallenbach@gmail.com>
2017-09-05 12:00:15 +0200

    Created Event Model

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Codaisseurup
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     console do
-      ActiveRecord::Base.conection
+      ActiveRecord::Base.connection
     end
     # Don't generate system test files.
     config.generators.system_tests = nil
